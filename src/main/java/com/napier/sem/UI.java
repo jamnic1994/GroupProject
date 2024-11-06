56a3afc74133b6cbefce96a56572835b217fa209
Zach <40592313@live.napier.ac.uk>
2024-11-06 15:48:37 +0000

    More UI Adjustments.

--- src/main/java/com/napier/sem/UI.java
+++ src/main/java/com/napier/sem/UI.java
@@ -101,14 +101,7 @@ public class UI {
 
         while (inputNum != -1) {
 
-            if(topPopulated){
-                System.out.println("Top N populated cities from largest to smallest: \n");
-            }
-            else{
-                System.out.println("Cities from largest to smallest: \n");
-            }
-
-            System.out.print("\n ______________________________\n");
+            System.out.print("\n______________________________\n");
             System.out.println(
                     "(1) Capital cities in world \n" +
                     "(2) Capital cities in region \n" +
