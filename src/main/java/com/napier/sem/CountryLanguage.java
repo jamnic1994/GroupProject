f9063cc70d1bd28f0d68f74c9fd7c9306ffc0148
jamnic1994 <jamie.nic1@gmail.com>
2024-11-22 13:29:30 +0000

    Added country language object class

---
+++ src/main/java/com/napier/sem/CountryLanguage.java
@@ -0,0 +1,28 @@
+package com.napier.sem;
+
+/**
+ * Class to represent a countrys language as an object
+ */
+public class CountryLanguage {
+
+    /**
+     * Country's assigned code
+     */
+    public String countryCode;
+
+    /**
+     * Country's language
+     */
+    public String language;
+
+    /**
+     * Is the language the country's official language
+     */
+    public String isOfficial;
+
+    /**
+     * Percentage of the country that speak this language
+     */
+    public String percentage;
+
+}
